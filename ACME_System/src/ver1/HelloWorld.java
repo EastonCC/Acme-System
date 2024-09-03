796f34657bbd1c9baa1d754705b1fafa355f6fa5
Easton Conkin <ceconkin@valdosta.edu>
2024-09-03 03:30:00 -0400

    HeyNow added!

--- ACME_System/src/ver1/HelloWorld.java
+++ ACME_System/src/ver1/HelloWorld.java
@@ -7,4 +7,9 @@ public class HelloWorld {
 		System.out.println("Easton is working locally on his branch");
 	}
 
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
+	
 }
